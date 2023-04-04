f7748082a57f018856174742a93dbaaac5689848
Burd44 <92385814+Burd44@users.noreply.github.com>
2023-04-04 13:37:38 +0300

    Update Ex2.R

--- Ex2.R
+++ Ex2.R
@@ -1,11 +1,11 @@
-p=as.numeric(readline())
+n=as.numeric(readline())
 10
-l=as.numeric(readline())
+p=as.numeric(readline())
 0.5
-geo = function(l, p)
+geo = function(p, n)
 {
-  pr=dgeom(1:p, l)
-  for (i in 1:p)
+  pr=dgeom(1:n, p)
+  for (i in 1:n)
   {
     print(pr[i])
   }
