c8bda01f0271080e5014f6e86b5e9170784a9e4a
Burd44 <92385814+Burd44@users.noreply.github.com>
2023-04-25 13:30:13 +0300

    Update Ex2.R

--- Ex2.R
+++ Ex2.R
@@ -1,13 +1,11 @@
-n=as.numeric(readline())
-10
-p=as.numeric(readline())
-0.5
-geo = function(p, n)
-{
-  pr=dgeom(1:n, p)
-  for (i in 1:n)
-  {
-    print(pr[i])
+s = sd(sample)
+outliers_mean <- mean(sample)
+lower <- outliers_mean - 2 * s
+higher <- outliers_mean + 2 * s
+outliers <- c()
+for (value in sample){
+  if (value < lower | value > higher){
+    outliers <- append(outliers, value)
   }
-  barplot(pr)
 }
+outliers
