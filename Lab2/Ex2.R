1ebd64775bc297e2fb181fc874a9fcf7af414365
Burd44 <92385814+Burd44@users.noreply.github.com>
2023-04-25 13:30:48 +0300

    Update Ex2.R

--- Lab2/Ex2.R
+++ Lab2/Ex2.R
@@ -1,11 +1,7 @@
-sample <- readLines("sample2.txt")
-sample = as.numeric(sample)
-outliers_iqr <- IQR(sample)
-samplesummary <- summary(sample)
-q1 <- samplesummary[2]
-q3 <- samplesummary[4]
-lower <- q1 - 1.5 * outliers_iqr
-higher <- q3 + 1.5 * outliers_iqr
+s = sd(sample)
+outliers_mean <- mean(sample)
+lower <- outliers_mean - 2 * s
+higher <- outliers_mean + 2 * s
 outliers <- c()
 for (value in sample){
   if (value < lower | value > higher){
