1d5f6df2689b8b96f32bb957d571e1834fd1ca39
Burd44 <92385814+Burd44@users.noreply.github.com>
2023-05-29 11:10:05 +0300

    Add files via upload

---
+++ Teme/Tema5.R
@@ -0,0 +1,63 @@
+#Exercitiul 1
+
+sample_mean = 138
+n = 10
+sigma = 11
+
+#90%
+alfa = 1 - 0.9
+critical_z = qnorm(1 - alfa/2, 0, 1)
+a = sample_mean - critical_z * sigma / sqrt(n)
+b = sample_mean + critical_z * sigma / sqrt(n)
+interval = c(a,b)
+interval
+
+#95%
+alfa = 1 - 0.95
+critical_z = qnorm(1 - alfa/2, 0, 1)
+a = sample_mean - critical_z*sigma / sqrt(n)
+b = sample_mean + critical_z*sigma / sqrt(n)
+interval = c(a,b)
+interval
+
+#99%
+alfa = 1 - 0.99
+critical_z = qnorm(1 - alfa/2, 0, 1)
+a = sample_mean - critical_z * sigma / sqrt(n)
+b = sample_mean + critical_z * sigma / sqrt(n)
+interval = c(a,b)
+interval
+
+#Exercitiul 2
+
+alfa = 1 - 0.95
+sample_mean = 18
+n = 256
+s = 1.2
+se = s/sqrt(n)
+critical_t = qt(1 - alfa/2, n - 1)
+a = sample_mean - critical_t*se
+b = sample_mean + critical_t*se
+interval = c(a,b)
+interval
+
+#Exercitiul 3
+
+n <- 153
+succese <- 17
+p_prim <- succese/n
+p_0 <- 0.12
+
+#1%
+alfa <- 0.01
+z_score <- (p_prim - p_0) / sqrt(p_0 * (1 - p_0) / n)
+critical_z <- qnorm(1 - alfa, 0, 1)
+z_score
+critical_z
+
+#5%
+alfa <- 0.05
+z_score <- (p_prim - p_0) / sqrt(p_0 * (1 - p_0) / n)
+critical_z <- qnorm(1 - alfa, 0, 1)
+z_score
+critical_z
